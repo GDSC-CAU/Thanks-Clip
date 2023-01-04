d1a218f9c7af45fa32d1d051435cccf706bbe9aa
Danpa_cho <danpa725@cau.ac.kr>
2023-01-04 09:47:53 +0900

    feat: export atoms with store

---
+++ src/atoms/index.js
@@ -0,0 +1,11 @@
+import { letter } from "./letter"
+import { letterText } from "./letterText"
+import { video } from "./video"
+
+const store = {
+    letter,
+    letterText,
+    video,
+}
+
+export { store }
