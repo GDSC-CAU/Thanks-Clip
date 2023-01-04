5ecccddc9456c9d30b1131e4e9fe15dcfbe55f32
Danpa_cho <danpa725@cau.ac.kr>
2023-01-04 09:14:57 +0900

    feat: basic tailwindcss test

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,29 @@ function App() {
     const minus = () => setCount((c) => c - 1)
 
     return (
-        <>
-            <h1>🔥 Final Project is here 🔥</h1>
+        <div className="py-5 flex flex-col items-center justify-center gap-4 h-screen">
+            <h1 className="text-5xl font-black mb-16">Final Project is here</h1>
 
-            <button type="button" onClick={plus}>
-                Plus
-            </button>
+            <div className="text-xl font-bold">Count is {count}</div>
 
-            <div>Count is, {count}</div>
+            <div className="flex flex-row items-center justify-center gap-4">
+                <button
+                    className="px-4 py-2 rounded ring-2 ring-red-200 bg-red-500 text-white active:translate-y-[1.25px]"
+                    type="button"
+                    onClick={minus}
+                >
+                    Plus
+                </button>
 
-            <button type="button" onClick={minus}>
-                Minus
-            </button>
-        </>
+                <button
+                    className="px-4 py-2 rounded ring-2 ring-blue-200 bg-blue-500 text-white active:translate-y-[1.25px]"
+                    type="button"
+                    onClick={plus}
+                >
+                    Minus
+                </button>
+            </div>
+        </div>
     )
 }
 
