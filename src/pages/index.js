b5bfb84f24d283494f1c283626d47c0c9f54a786
Danpa_cho <danpa725@cau.ac.kr>
2023-01-06 10:36:24 +0900

    docs: add page README.md

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as Home } from "./home"
+export { default as Step1 } from "./step1"
+export { default as Step2 } from "./step2"
+export { default as Step3 } from "./step3"
+export { default as Step4 } from "./step4"
+export { default as Error } from "./error"
