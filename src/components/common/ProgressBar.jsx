13e2a00dfc7c153550260f9d1a82cf5bd9d51438
1lsang <ilsangpark.98@gmail.com>
2023-01-07 00:38:53 +0900

    feat: add ProgressBar common component

--- src/components/common/ProgressBar.jsx
+++ src/components/common/ProgressBar.jsx
@@ -1,5 +1,21 @@
-const ProgressBar = () => {
-    return <div>ProgressBar</div>
+import { useLocation } from "react-router-dom"
+// import { LinkTo } from "../../router"
+
+const ProgressBar = ({ ...progressBarProps }) => {
+    const step = useLocation().pathname.split("/").slice(-1)[0]
+
+    return (
+        <div {...progressBarProps}>
+            <div className="w-full h-2 bg-gray-200 rounded-full">
+                <div
+                    className={`transition transition-all duration-300 h-full bg-red-400 rounded-full w-full ${`w-${step}/4`}`}
+                ></div>
+            </div>
+            {/* Link Component for Test */}
+            {/* <LinkTo to={`/step/${Number(step) - 1}`}>Step1</LinkTo> */}
+            {/* <LinkTo to={`/step/${Number(step) + 1}`}>Step2</LinkTo> */}
+        </div>
+    )
 }
 
 export { ProgressBar }
