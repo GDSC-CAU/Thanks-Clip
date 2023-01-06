b27e5096bd1519ec9dae6331ecd984f0f48d0b1b
Koeunseooooo <rhdmstj17@cau.ac.kr>
2023-01-06 17:14:06 +0900

    feat: add onClick event to PrevButton

--- src/components/common/PrevButton.jsx
+++ src/components/common/PrevButton.jsx
@@ -1,8 +1,10 @@
 import { ChevronLeftIcon } from "@heroicons/react/20/solid"
+import { useNavigate } from "react-router-dom"
 
 const PrevButton = () => {
+    const navigate = useNavigate()
     return (
-        <button>
+        <button onClick={() => navigate(-1)}>
             <ChevronLeftIcon className="h-10 w-10" />
         </button>
     )
