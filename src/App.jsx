d7fe856e7552fc8ecdc0db560a7fb372a72628a1
Danpa_cho <danpa725@cau.ac.kr>
2023-01-06 16:31:48 +0900

    refactor: move router provider to App and add App container

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import { RouterProvider } from "./router"
+import { useEffect } from "react"
+import { RouterProvider } from "react-router-dom"
+import { router, routes } from "./router"
 
-const App = () => {
+const AppContainer = (props) => (
+    <div className="bg-gray-50">{props.children}</div>
+)
+
+const App = (props) => {
+    useEffect(() => {
+        const shouldReplaceInvalidAccess =
+            window.location.pathname === routes.stepCommon
+
+        if (shouldReplaceInvalidAccess) {
+            window.location.replace(routes.home)
+        }
+    }, [])
+
     return (
-        <div className="bg-gray-50">
-            <RouterProvider />
-        </div>
+        <AppContainer>
+            <RouterProvider router={router}>{props.children}</RouterProvider>
+        </AppContainer>
     )
 }
 
