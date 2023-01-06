718483e153b8b367d95c1c30cbf66ce9b55e4c5c
Danpa_cho <danpa725@cau.ac.kr>
2023-01-06 10:38:27 +0900

    feat: render App

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Header from "./components/Header"
-import ProgressBar from "./components/ProgressBar"
-import Home from "./pages/Home"
-import Step1 from "./pages/Step1"
-import Step2 from "./pages/Step2"
-import Step3 from "./pages/Step3"
-import Step4 from "./pages/Step4"
-import Step5 from "./pages/Step5"
+import { Router } from "./router"
 
-const routes = {
-    home: "/",
-    step1: "/step1",
-    step2: "/step2",
-    step3: "/step3",
-    step4: "/step4",
-    step5: "/step5",
-}
-
-export { routes }
-
-function App() {
+const App = () => {
     return (
-        <>
-            <Header></Header>
-            <ProgressBar></ProgressBar>
-            <BrowserRouter>
-                <Routes>
-                    <Route exact path={routes.home} element={<Home />} />
-                    <Route path={routes.step1} element={<Step1 />} />
-                    <Route path={routes.step2} element={<Step2 />} />
-                    <Route path={routes.step3} element={<Step3 />} />
-                    <Route path={routes.step4} element={<Step4 />} />
-                    <Route path={routes.step5} element={<Step5 />} />
-                </Routes>
-            </BrowserRouter>
-        </>
+        <div className="bg-gray-50">
+            <Router />
+        </div>
     )
 }
 
-export default App
+export { App }
