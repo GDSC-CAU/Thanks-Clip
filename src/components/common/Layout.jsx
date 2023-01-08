de16d6a9fead35d9fb51691a5147631615b2cfa2
Danpa_cho <danpa725@cau.ac.kr>
2023-01-08 18:15:43 +0900

    refactor: add nav component for common layout

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,11 +1,24 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom"
+import { PrevButton } from "./PrevButton"
 import { ProgressBar } from "./ProgressBar"
 
-const Layout = () => (
-    <main className="w-full h-screen min-h-screen mx-auto sm:w-4/5 md:w-3/5 lg:w-2/5 px-3 py-4 bg-white">
-        <ProgressBar />
-        <Outlet />
-    </main>
-)
+const Nav = () => {
+    const step = useLocation().pathname.split("/").slice(-1)[0]
+    return (
+        <div className="flex flex-col items-start justify-center gap-1">
+            <PrevButton step={Number(step)} />
+            <ProgressBar step={step} />
+        </div>
+    )
+}
+
+const Layout = () => {
+    return (
+        <main className="w-full h-screen min-h-screen mx-auto sm:w-4/5 md:w-3/5 lg:w-2/5 px-3 py-3 bg-white">
+            <Nav />
+            <Outlet />
+        </main>
+    )
+}
 
 export { Layout }
