bc0f972ed1430ce3b1a201892ff3a630acf8878e
Koeunseooooo <rhdmstj17@cau.ac.kr>
2023-01-08 11:47:41 +0900

    feat: add keyword selection func to step2 page

--- src/pages/step2/index.jsx
+++ src/pages/step2/index.jsx
@@ -1,3 +1,21 @@
-export default function Step2() {
-    return <div>Step2</div>
+import { useState } from "react"
+import { KeywordButton } from "../../components/pages/KeywordButton"
+import { KeywordTextList } from "../../components/pages/KeywordTextList"
+
+export default function Step1() {
+    const [KeywordBtnActive, setKeywordBtnActive] = useState(null)
+    const toggleActive = (idx) => setKeywordBtnActive(idx)
+    return (
+        <div>
+            {KeywordTextList.map((item, idx) => (
+                <KeywordButton
+                    key={idx}
+                    id={idx}
+                    isSelected={idx === KeywordBtnActive}
+                    keywordText={item.keywordText}
+                    onClick={toggleActive}
+                ></KeywordButton>
+            ))}
+        </div>
+    )
 }
