9973edc579d32cc8f2d1fb20b5b87c8e8d0cbcca
Koeunseooooo <rhdmstj17@cau.ac.kr>
2023-01-08 20:58:51 +0900

    refactor: rename keywordBtnActive to isSelected

--- src/pages/step2/index.jsx
+++ src/pages/step2/index.jsx
@@ -1,29 +1,43 @@
 import { useState } from "react"
+import { Button } from "../../components/common/Button"
 import { KeywordButton } from "../../components/pages/KeywordButton"
 import { KeywordTextList } from "../../components/pages/KeywordTextList"
 
 export default function Step2() {
-    const [keywordBtnActive, setKeywordBtnActive] = useState(
+    // const [activeKeywordNum, setActiveKeywordNum] = useState(0)
+    const [isSelected, setSelect] = useState(
         new Array(KeywordTextList.length).fill(false)
     )
+    // const countActiveKeywordNum = () => {
+    //     setActiveKeywordNum(
+    //         keywordBtnActive.filter((element) => true === element).length
+    //     )
+    // }
+
     const handleKeywordButton = (idx) => {
-        setKeywordBtnActive([
-            ...keywordBtnActive.slice(0, idx),
-            !keywordBtnActive[idx],
-            ...keywordBtnActive.slice(idx + 1),
+        setSelect([
+            ...isSelected.slice(0, idx),
+            !isSelected[idx],
+            ...isSelected.slice(idx + 1),
         ])
+        // console.log(
+        //     keywordBtnActive.filter((element) => true === element).length
+        // )
+        // countActiveKeywordNum()
     }
+
     return (
         <div>
             {KeywordTextList.map((item, idx) => (
                 <KeywordButton
                     key={idx}
                     id={idx}
-                    isSelected={keywordBtnActive[idx]}
+                    isSelected={isSelected[idx]}
                     keywordText={item.keywordText}
                     onClick={handleKeywordButton}
                 ></KeywordButton>
             ))}
+            <Button>계속하기</Button>
         </div>
     )
 }
