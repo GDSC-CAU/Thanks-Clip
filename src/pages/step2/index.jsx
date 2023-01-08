30311adae5c2278c72d7e635d23ff94e054a2e0c
Koeunseooooo <rhdmstj17@cau.ac.kr>
2023-01-08 21:23:17 +0900

    feat: add useEffect to count activeKeywordNum

--- src/pages/step2/index.jsx
+++ src/pages/step2/index.jsx
@@ -1,18 +1,28 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Button } from "../../components/common/Button"
 import { KeywordButton } from "../../components/pages/KeywordButton"
 import { KeywordTextList } from "../../components/pages/KeywordTextList"
 
 export default function Step2() {
-    // const [activeKeywordNum, setActiveKeywordNum] = useState(0)
+    const [activeKeywordNum, setActiveKeywordNum] = useState(0)
     const [isSelected, setSelect] = useState(
         new Array(KeywordTextList.length).fill(false)
     )
-    // const countActiveKeywordNum = () => {
-    //     setActiveKeywordNum(
-    //         keywordBtnActive.filter((element) => true === element).length
-    //     )
-    // }
+    useEffect(() => {
+        countActiveKeywordNum()
+    }, [isSelected])
+
+    useEffect(() => {
+        if (activeKeywordNum >= 5) {
+            console.log("최대 5개까지만 추가할 수 있습니다")
+        }
+    }, [activeKeywordNum])
+
+    const countActiveKeywordNum = () => {
+        setActiveKeywordNum(
+            isSelected.filter((element) => true === element).length
+        )
+    }
 
     const handleKeywordButton = (idx) => {
         setSelect([
@@ -20,10 +30,6 @@ export default function Step2() {
             !isSelected[idx],
             ...isSelected.slice(idx + 1),
         ])
-        // console.log(
-        //     keywordBtnActive.filter((element) => true === element).length
-        // )
-        // countActiveKeywordNum()
     }
 
     return (
