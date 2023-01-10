d4f2da66c339cfe8f6d29e2374fe43af885e043f
Danpa_cho <danpa725@cau.ac.kr>
2023-01-10 23:16:19 +0900

    feat: add deploy config constant

--- constant/deployConfig.js
+++ constant/deployConfig.js
@@ -5,21 +5,19 @@
     RAM: number;
     DISK: number;
     TIMEOUT: number;
-    DEPLOY_REGION: "ap-northeast-2";
-    SITE_NAME: string;
-    VIDEO_ENTRY_POINT: string;
+    SITE_NAME: "thanks-clip";
+    VIDEO_ENTRY_POINT: "video/index.jsx";
     VIDEO_COMPOSITION_ID: "thanks-clip";
 }} 
 */
-const CONFIG = {
+const DEPLOY_CONFIG = {
     DURATION: 1100,
-    RAM: 2048,
+    RAM: 2048, //!TODO: 배포전 체크리스트 확인 https://www.remotion.dev/docs/lambda/checklist
     DISK: 2048,
     TIMEOUT: 240,
-    VIDEO_COMPOSITION_ID: "thanks-clip",
-    DEPLOY_REGION: "ap-northeast-2",
     SITE_NAME: "thanks-clip",
-    VIDEO_ENTRY_POINT: "src/video/index.jsx",
+    VIDEO_COMPOSITION_ID: "thanks-clip",
+    VIDEO_ENTRY_POINT: "video/index.jsx",
 }
 
-export { CONFIG }
+export { DEPLOY_CONFIG }
