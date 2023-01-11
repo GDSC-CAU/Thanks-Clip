7b34f5713b71c6a216394c6984718dfb3394d997
Danpa_cho <danpa725@cau.ac.kr>
2023-01-11 17:41:59 +0900

    refactor: extract size into props for video customized prop rendering

--- app/step/3/components/Letter/Canvas.jsx
+++ app/step/3/components/Letter/Canvas.jsx
@@ -2,25 +2,6 @@
 
 import { useEffect, useState } from "react"
 
-const useCanvasSize = ({ containerRef }) => {
-    const [size, setSize] = useState(0)
-
-    useEffect(() => {
-        setSize(containerRef.current.offsetWidth)
-
-        const resizeHandler = () => {
-            setSize(containerRef.current.offsetWidth)
-        }
-
-        window.addEventListener("resize", resizeHandler)
-        return () => window.removeEventListener("resize", resizeHandler)
-    }, [containerRef])
-
-    return {
-        size: `${size}px`,
-    }
-}
-
 const useMounted = () => {
     const [isMounted, setIsMounted] = useState(false)
 
@@ -31,12 +12,11 @@ const useMounted = () => {
     return { isMounted }
 }
 
-const Canvas = ({ children, containerRef }) => {
+const Canvas = ({ children, size }) => {
     const { isMounted } = useMounted()
-    const { size } = useCanvasSize({ containerRef })
     const canvasSize = {
-        width: size,
-        height: size,
+        width: `${size}px`,
+        height: `${size}px`,
     }
 
     return (
--- app/step/3/components/Letter/index.jsx
+++ app/step/3/components/Letter/index.jsx
@@ -1,8 +1,27 @@
 "use client"
 
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { Canvas } from "./Canvas"
 import { EditorToolBar, EditorToolBarSelector } from "./Editor"
+
+const useCanvasSize = ({ containerRef }) => {
+    const [size, setSize] = useState(0)
+
+    useEffect(() => {
+        setSize(containerRef.current.offsetWidth)
+
+        const resizeHandler = () => {
+            setSize(containerRef.current.offsetWidth)
+        }
+
+        window.addEventListener("resize", resizeHandler)
+        return () => window.removeEventListener("resize", resizeHandler)
+    }, [containerRef])
+
+    return {
+        size,
+    }
+}
 
 const Letter = () => {
     const [selectedOption, setSelectedOption] = useState("")
@@ -12,15 +31,14 @@ const Letter = () => {
     }
 
     const letterContainerRef = useRef()
+    const { size } = useCanvasSize({ containerRef: letterContainerRef })
 
     return (
         <div
             ref={letterContainerRef}
             className="flex items-center justify-center h-fit w-full relative"
         >
-            <Canvas containerRef={letterContainerRef}>
-                이곳에 그려봅시다!
-            </Canvas>
+            <Canvas size={size}>이곳에 그려봅시다!</Canvas>
 
             <EditorToolBarSelector
                 handleClickOptions={handleClickOptions}
