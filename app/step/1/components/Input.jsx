02fd8ffdc1f9aacd7a5634358f29da760cbb304d
Danpa_cho <danpa725@cau.ac.kr>
2023-01-12 01:14:42 +0900

    refactor: Input and Textarea component prop design and minor style

--- app/step/1/components/Input.jsx
+++ app/step/1/components/Input.jsx
@@ -1,18 +1,11 @@
-"use client"
-
-const inputStyle =
-    "border-b-2 border-neutral-300 outline-0 focus:border-neutral-500"
-
-const Input = ({ value, onChange }) => {
-    return (
-        <input
-            className={inputStyle}
-            type="text"
-            maxLength={10}
-            value={value}
-            onChange={onChange}
-        />
-    )
-}
+const Input = ({ value, onChange }) => (
+    <input
+        className="border-b-[1.5px] border-neutral-300 outline-0 focus:border-neutral-500 transition pb-1"
+        type="text"
+        maxLength={10}
+        value={value}
+        onChange={onChange}
+    />
+)
 
 export { Input }
--- app/step/1/components/Textarea.jsx
+++ app/step/1/components/Textarea.jsx
@@ -1,17 +1,12 @@
-"use client"
-
-const Textarea = ({ onChange, children }) => {
-    return (
-        <textarea
-            className="border-2 border-neutral-300 rounded-xl p-1 outline-0 focus:border-neutral-500"
-            maxLength={100}
-            rows="15"
-            cols="5"
-            onChange={onChange}
-        >
-            {children}
-        </textarea>
-    )
-}
+const Textarea = ({ onChange, value }) => (
+    <textarea
+        className="border-[1.25px] border-neutral-300 rounded-lg p-3 outline-0 focus:border-neutral-500 select-none resize-none transition"
+        maxLength={100}
+        rows="10"
+        cols="5"
+        onChange={onChange}
+        value={value}
+    />
+)
 
 export { Textarea }
