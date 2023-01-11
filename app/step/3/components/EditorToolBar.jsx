e9aa8924fe429feaaa9a7a88cf2f5b34940fdab2
Danpa_cho <danpa725@cau.ac.kr>
2023-01-11 14:17:08 +0900

    refactor: EditorToolBox transform animation

--- app/step/3/components/EditorToolBar.jsx
+++ app/step/3/components/EditorToolBar.jsx
@@ -1,11 +1,16 @@
 import { Container } from "./Container"
 const EditorToolBar = (props) => {
+    const isActive = props.selectedOption !== ""
     return (
-        <div className="absolute bottom-3 left-1/2 -translate-x-1/2">
-            {props.selectedOption && (
-                <Container isEditorToolBar>{props.selectedOption}</Container>
-            )}
-        </div>
+        <Container
+            className={`flex items-center justify-center h-16 max-h-[4rem] ${
+                isActive
+                    ? "opacity-100 scale-100 translate-y-0 pointer-events-auto"
+                    : "opacity-0 scale-0 translate-y-12 pointer-events-none"
+            } absolute bottom-3 left-1/2 -translate-x-1/2 transition-all duration-300`}
+        >
+            {props.selectedOption}
+        </Container>
     )
 }
 
