d2d0d33ea76ff610eb41b0985f2bd02da405ec1c
Danpa_cho <danpa725@cau.ac.kr>
2023-01-12 01:14:12 +0900

    fix: button disabled bg-color style error

--- app/common/Button.jsx
+++ app/common/Button.jsx
@@ -1,17 +1,17 @@
 const button =
-    "flex items-center justify-center gap-1 px-7 py-3 text-lg border-solid border-2 rounded-full"
+    "flex items-center justify-center gap-1 px-7 py-3 text-lg border-solid border-2 rounded-full transition"
 
-const buttonDisabled =
-    "bg-gray-300 text-gray-600 border-gray-300 opacity-50 cursor-not-allowed"
+const buttonDisabled = "opacity-50 cursor-not-allowed"
 
 const buttonColor = {
     red: "bg-red-400 text-white border-red-400",
     white: "bg-white text-gray-800 border-gray-800",
     gray: "bg-gray-300 text-white border-gray-300",
+    lightGray: "bg-gray-300 text-gray-600 border-gray-300 ",
 }
 
 /**
- * @param {{ children: React.ReactNode, onClick: React.MouseEventHandler<HTMLButtonElement>, size?: "full" | "fit", color?: "red" | "white" | "gray", disabled?: true | false, className?: string }} props
+ * @param {{ children: React.ReactNode, onClick: React.MouseEventHandler<HTMLButtonElement>, size?: "full" | "fit", color?: "red" | "white" | "gray" | "lightGray", disabled?: true | false, className?: string }} props
  */
 const Button = ({
     children,
@@ -24,7 +24,7 @@ const Button = ({
     const buttonSize = size === "full" ? "w-full" : "w-fit"
 
     const buttonStyle = `${button} ${disabled ? buttonDisabled : ""} ${
-        buttonColor[color]
+        buttonColor[disabled ? "lightGray" : color]
     } ${buttonSize} ${className ?? ""}`
 
     return (
