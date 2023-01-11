d2a7a0425548d9121453adbf7656e66367f4899b
Koeunseooooo <rhdmstj17@cau.ac.kr>
2023-01-11 12:20:15 +0900

    fix: fir error when EditorToolBar is visible when option is not clicked

--- app/step/3/components/EditorToolBar.jsx
+++ app/step/3/components/EditorToolBar.jsx
@@ -2,7 +2,9 @@ import { Container } from "./Container"
 const EditorToolBar = (props) => {
     return (
         <div className="absolute bottom-3 left-1/2 -translate-x-1/2">
-            <Container isEditorToolBar>{props.selectedOption}</Container>
+            {props.selectedOption && (
+                <Container isEditorToolBar>{props.selectedOption}</Container>
+            )}
         </div>
     )
 }
