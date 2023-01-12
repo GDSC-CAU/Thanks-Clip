a992478b8297a7f010f2d22234961515afb06fa6
Koeunseooooo <rhdmstj17@cau.ac.kr>
2023-01-12 10:58:21 +0900

    chore: install @svgr/webpack & modify next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
     experimental: {
         appDir: true,
     },
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ["@svgr/webpack"],
+        })
+        return config
+    },
 }
 
 export default nextConfig
