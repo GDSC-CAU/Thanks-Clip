a71701cfa06e4856e7a2515006986fe97afe323d
Koeunseooooo <rhdmstj17@cau.ac.kr>
2023-01-13 22:46:12 +0900

    feat: add keyword texts

--- app/step/2/components/KeywordButton.jsx
+++ app/step/2/components/KeywordButton.jsx
@@ -1,5 +1,5 @@
 const keywordBtn =
-    "max-h-11 py-1.5 text-md border-solid border-2 rounded-full bg-gray-300 text-gray-500 border-gray-300"
+    "h-12 py-1.5 text-md border-solid border-2 rounded-full bg-gray-300 text-gray-500 border-gray-300"
 
 const keywordBtnSelected =
     "py-1.5 text-md border-solid border-2 rounded-full bg-red-400 text-white border-red-400 shadow-lg shadow-red-400/25"
@@ -8,7 +8,7 @@ const KeywordButton = ({ children, id, isSelected, onClick }) => {
     const keywordBtnStyle = `${isSelected ? keywordBtnSelected : keywordBtn}`
     return (
         <button
-            className={`hover:scale-[101%] active:scale-95 transition ${keywordBtnStyle}`}
+            className={`w-full text-sm hover:scale-[101%] active:scale-95 transition ${keywordBtnStyle}`}
             onClick={() => onClick(id)}
         >
             {children}
