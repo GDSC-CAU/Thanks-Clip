61e62006bf7e023d8cc8499c27eed537f744ec67
Koeunseooooo <rhdmstj17@cau.ac.kr>
2023-01-14 00:00:06 +0900

    fix: fix css style when button is active

--- app/step/2/components/KeywordButton.jsx
+++ app/step/2/components/KeywordButton.jsx
@@ -1,5 +1,5 @@
 const keywordBtn =
-    "h-12 py-1.5 text-md border-solid border-2 rounded-full bg-gray-300 text-gray-500 border-gray-300"
+    "py-1.5 text-md border-solid border-2 rounded-full bg-gray-300 text-gray-500 border-gray-300"
 
 const keywordBtnSelected =
     "py-1.5 text-md border-solid border-2 rounded-full bg-red-400 text-white border-red-400 shadow-lg shadow-red-400/25"
@@ -8,7 +8,7 @@ const KeywordButton = ({ children, id, isSelected, onClick }) => {
     const keywordBtnStyle = `${isSelected ? keywordBtnSelected : keywordBtn}`
     return (
         <button
-            className={`w-full text-sm hover:scale-[101%] active:scale-95 transition ${keywordBtnStyle}`}
+            className={`h-12 w-full text-sm hover:scale-[101%] active:scale-95 transition ${keywordBtnStyle}`}
             onClick={() => onClick(id)}
         >
             {children}
