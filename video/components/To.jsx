aeefd354f31d1defc8eda10646b8b44f89525c2f
1lsang <ilsangpark.98@gmail.com>
2023-01-14 02:53:28 +0900

    feat: add To component for video rendring

---
+++ video/components/To.jsx
@@ -0,0 +1,50 @@
+import {
+    AbsoluteFill,
+    interpolate,
+    useCurrentFrame,
+    useVideoConfig,
+} from "remotion/."
+
+const DURATION_TO = 60
+
+const To = ({ delay, children }) => {
+    const frame = useCurrentFrame()
+    const { fps } = useVideoConfig()
+    const opacity = interpolate(
+        frame,
+        [
+            delay,
+            delay + fps / 2,
+            delay + DURATION_TO - fps / 2,
+            delay + DURATION_TO,
+        ],
+        [0, 1, 1, 0]
+    )
+
+    const translateY = interpolate(
+        frame,
+        [
+            delay,
+            delay + fps / 2,
+            delay + DURATION_TO - fps / 2,
+            delay + DURATION_TO,
+        ],
+        [1, 0, 0, 1]
+    )
+
+    return (
+        <AbsoluteFill
+            style={{
+                fontSize: "3rem",
+                top: "calc(50% - 1.5rem)",
+                textAlign: "center",
+                opacity: opacity,
+                transform: `translateY(${translateY}rem)`,
+            }}
+        >
+            {children}에게
+        </AbsoluteFill>
+    )
+}
+
+export { To, DURATION_TO }
