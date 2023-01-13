20713e80a392284dbc682a17746fa04147f1d604
likeyeon <chy98csy00@naver.com>
2023-01-13 19:33:24 +0900

    Revert "fix: delete step0"
    This reverts commit 68c8f398427c6929c7390e094546f155c4c53546.

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import Link from "next/link"
 import { Button } from "./common/Button"
 
@@ -5,6 +6,22 @@ export default function Main() {
     return (
         <main className="flex flex-col w-full h-screen min-h-screen mx-auto sm:w-4/5 md:w-3/5 lg:w-2/5 px-3 py-3 bg-white">
             <div className="flex flex-col h-full justify-between">
+                <Image
+                    className="my-6"
+                    src={"/logo/2line_red.png"}
+                    alt="love"
+                    width={500}
+                    height={500}
+                />
+                <div className={`flex flex-col gap-1.5 font-main-Sans`}>
+                    <div className="flex justify-center text-2xl font-bold ">
+                        Send your&nbsp;
+                        <span className={`text-red-400`}>Thanks</span>
+                    </div>
+                    <div className="flex justify-center text-neutral-400">
+                        편지를 통해 감사한 마음을 전해 보세요
+                    </div>
+                </div>
                 <Link href="/step/1">
                     <Button className="mt-1 mb-12">시작해볼까요?</Button>
                 </Link>
