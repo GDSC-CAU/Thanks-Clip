aa4c526fa673548c1b055ce25889434c9cbfb86d
Danpa_cho <danpa725@cau.ac.kr>
2023-01-14 20:11:21 +0900

    feat: minor update of editor toolbar appear motion

--- app/step/3/components/Letter/Editor/ToolBar.jsx
+++ app/step/3/components/Letter/Editor/ToolBar.jsx
@@ -9,7 +9,7 @@ const EditorToolBar = ({ selectedOption }) => {
             className={`flex items-center justify-center h-16 max-h-[4rem] z-50 ${
                 isActive
                     ? "opacity-100 scale-100 translate-y-0 pointer-events-auto"
-                    : "opacity-0 scale-0 translate-y-32 pointer-events-none"
+                    : "opacity-0 scale-0 translate-y-4 pointer-events-none"
             } absolute -bottom-10 left-1/2 -translate-x-1/2 transition-all duration-300`}
         >
             {selectedOption === "font" && <FontToolBar />}
