d748127136bff20c99e6953ef38a48d0ebb7aac2
1lsang <ilsangpark.98@gmail.com>
2023-01-14 22:59:32 +0900

    fix: fix input appearance for ios safari browser

--- app/step/1/components/Input.jsx
+++ app/step/1/components/Input.jsx
@@ -1,6 +1,6 @@
 const Input = ({ value, onChange }) => (
     <input
-        className="border-b-[1.5px] border-neutral-300 outline-none focus:border-neutral-500 transition pb-1"
+        className="border-b-[1.5px] border-neutral-300 outline-none focus:border-neutral-500 transition pb-1 [appearance:none] [-moz-appearance:none] [-webkit-appearance:none] rounded-none [-webkit-border-radius:0] [-moz-border-radius:0]"
         type="text"
         maxLength={10}
         value={value}
