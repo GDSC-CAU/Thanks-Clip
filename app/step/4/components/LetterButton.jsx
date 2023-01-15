fb03791d38a348f520a5b9eac045bedbda859bc2
Danpa_cho <danpa725@cau.ac.kr>
2023-01-15 21:38:50 +0900

    fix: error of not removed stickers data

--- app/step/4/components/LetterButton.jsx
+++ app/step/4/components/LetterButton.jsx
@@ -5,17 +5,27 @@ import { RESET } from "jotai/utils"
 import Link from "next/link"
 import { store } from "../../../../atoms"
 import { Button } from "../../../common/Button"
+import { KakaoShareLink } from "./KakaoShareLink"
 
 const LetterButton = () => {
     const setLetter = useSetAtom(store.letter)
+    const setStickers = useSetAtom(store.stickers)
+
+    const reset = () => {
+        setLetter(RESET)
+        setStickers([])
+    }
 
     return (
         <div className="mb-auto pt-10">
-            <Button className="mb-5">카카오톡으로 Clip 보내기</Button>
+            <KakaoShareLink urlParams={{}} onClick={reset}>
+                <Button className="mb-5">카카오톡으로 Clip 보내기</Button>
+            </KakaoShareLink>
+
             <Link href="/">
                 <Button
                     className="bg-inherit text-gray-800 border-gray-800 border-solid border"
-                    onClick={() => setLetter(RESET)}
+                    onClick={reset}
                 >
                     한개 더 만들기
                 </Button>
