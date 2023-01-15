1f70d40531f1f56d0e922d85fc9557d969aead26
Danpa_cho <danpa725@cau.ac.kr>
2023-01-15 18:19:04 +0900

    fix: correct import in page4

--- app/step/4/page.js
+++ app/step/4/page.js
@@ -1,17 +1,18 @@
 import { cookies } from "next/headers"
 import { encodeVideo } from "../../../pages/api/rendering.js"
+// import { KakaoShareLink } from "./components/KakaoShareLink.jsx"
 import { LetterButton } from "./components/LetterButton"
 import { LetterToName } from "./components/LetterToName"
-import { KakaoShareLink } from "./components/KakaoShareLink.jsx"
 
-export default function Step4() {
+export default async function Step4() {
     const nextCookies = cookies()
     const videoProps = JSON.parse(
         nextCookies.get("props")?.value ?? JSON.stringify({ name: "danpacho" })
     )
 
     const encode = await encodeVideo(videoProps)
-
+    // eslint-disable-next-line no-unused-vars
+    const { bucketName, region, renderId } = encode
     return (
         <>
             <LetterToName />
