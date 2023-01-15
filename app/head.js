711b99e2289af5c6194dd34dea6ecbdc13ff5ab4
1lsang <ilsangpark.98@gmail.com>
2023-01-15 09:04:52 +0900

    feat: add meta tag

--- app/head.js
+++ app/head.js
@@ -6,6 +6,29 @@ export default function Head() {
                 content="width=device-width, initial-scale=1"
                 name="viewport"
             />
+            <meta name="author" content="GDSC-CAU" />
+            <meta name="keywords" content="Thanks Clip" />
+            <meta
+                name="description"
+                content="짧은 동영상으로 고마운 마음을 전해볼까요?"
+            />
+
+            <meta property="og:url" content="https://" />
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content="Thanks Clip" />
+            <meta property="og:image" content="share/thanks_clip.jpg" />
+            <meta
+                property="og:description"
+                content="쉽고 빠르게 짧은 동영상을 만들어 고마운 마음을 전해볼까요?"
+            />
+            <meta name="twitter:url" content="https://" />
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:title" content="Thanks Clip" />
+            <meta name="twitter:image" content="share/thanks_clip.jpg" />
+            <meta
+                name="twitter:description"
+                content="쉽고 빠르게 짧은 동영상을 만들어 고마운 마음을 전해볼까요?"
+            />
             <link rel="icon" href="/favicon.ico" />
         </>
     )
