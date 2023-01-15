a5adf39babc01076f821ec1f17bebd73e7d6422b
Koeunseooooo <rhdmstj17@cau.ac.kr>
2023-01-15 11:36:28 +0900

    feat: add `KakaoShare` component & test: add example in test page

---
+++ app/test/ShareKakao.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react"
+const ShareKakao = () => {
+    useEffect(() => {
+        kakaoButton()
+    }, [])
+
+    const kakaoButton = () => {
+        if (window.Kakao) {
+            const kakao = window.Kakao
+            if (!kakao.isInitialized()) {
+                kakao.init(process.env.NEXT_PUBLIC_KAKAO_API_KEY) // 카카오에서 제공받은 javascript key를 넣어줌 -> .env파일에서 호출시킴
+            }
+            // createCustomButton()
+            kakao.Share.createCustomButton({
+                container: "#kakaotalk-sharing-btn",
+                templateId: 88654,
+                templateArgs: {
+                    title: "제목 영역입니다.",
+                    description: "설명 영역입니다.",
+                },
+            })
+        }
+    }
+    return (
+        <button id="kakaotalk-sharing-btn" onClick={kakaoButton}>
+            KakaoShare
+        </button>
+    )
+}
+
+export { ShareKakao }
---
+++ app/test/page.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/no-unresolved
+import { TestForm } from "./testForm"
+
+export default function Test() {
+    return <TestForm></TestForm>
+}
