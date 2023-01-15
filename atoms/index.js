99e0bd4509d2fb435e6219105888674aaea456f2
Danpa_cho <danpa725@cau.ac.kr>
2023-01-15 16:39:25 +0900

    refactor: remove unused name atom

--- atoms/index.js
+++ atoms/index.js
@@ -1,10 +1,8 @@
 import { letter } from "./letter"
-import { myName } from "./name"
 import { activeSticker, stickers } from "./sticker"
 
 const store = {
     letter,
-    myName,
     stickers,
     activeSticker,
 }
