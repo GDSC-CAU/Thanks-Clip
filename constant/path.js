09cb9654872d2c37df4cf1c3150fde317c256acf
Danpa_cho <danpa725@cau.ac.kr>
2023-01-16 23:31:58 +0900

    feat: update path base for deployment

--- constant/path.js
+++ constant/path.js
@@ -1,7 +1,7 @@
 const pathBase =
     process.env.NODE_ENV === "development"
         ? "http://localhost:3000"
-        : "http://localhost:3000"
+        : "https://thanksclip.vercel.app"
 
 //!TODO: 배포 주소로 변경하기
 
