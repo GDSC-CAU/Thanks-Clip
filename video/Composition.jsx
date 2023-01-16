8ec005995d1f05b357707c8588843e1623dec22f
Danpa_cho <danpa725@cau.ac.kr>
2023-01-16 16:18:08 +0900

    feat: video rendering props

--- video/Composition.jsx
+++ video/Composition.jsx
@@ -6,7 +6,21 @@ import { LogoSM } from "./components/LogoSM"
 import { DURATION_PAPER_PLANE, PaperPlane } from "./components/PaperPlane"
 import { DURATION_TO, To } from "./components/To"
 
-const RemotionComposition = ({ to, tags }) => {
+/**@typedef {import("../atoms/sticker.js").Sticker} Sticker */
+/**@typedef  {{size: number, to: string, letterTextSVG: string, stickers: Sticker[], letterType: "hole" | "overlap" | "torn", backgroundColor: "white" | "ivory" | "red" | "yellow" | "blue", tags: string[] }} LetterVideoProps */
+
+/**
+ * @param {LetterVideoProps} letterVideoProps
+ */
+const RemotionComposition = ({
+    to,
+    tags,
+    backgroundColor,
+    letterTextSVG,
+    letterType,
+    size,
+    stickers,
+}) => {
     return (
         <AbsoluteFill style={{ background: "white", padding: "1rem" }}>
             <Keywords keywords={tags} />
@@ -23,39 +37,11 @@ const RemotionComposition = ({ to, tags }) => {
                 }
             >
                 <Letter
-                    to={to}
-                    from="준성"
-                    font="cute"
-                    backgroundColor="yellow"
-                    letter="이제 동작을 하고 있습니다...!"
-                    letterType="torn"
-                    size={300}
-                    stickers={[
-                        {
-                            type: "star",
-                            position: {
-                                x: 266.0390625,
-                                y: 140.3671875,
-                            },
-                            isActive: false,
-                        },
-                        {
-                            type: "star",
-                            position: {
-                                x: 87.51171875,
-                                y: 24.52734375,
-                            },
-                            isActive: false,
-                        },
-                        {
-                            type: "heart",
-                            position: {
-                                x: 17.94140625,
-                                y: 66.7421875,
-                            },
-                            isActive: false,
-                        },
-                    ]}
+                    size={size}
+                    stickers={stickers}
+                    letterType={letterType}
+                    letterTextSVG={letterTextSVG}
+                    backgroundColor={backgroundColor}
                 />
             </Envelope>
             <LogoSM />
