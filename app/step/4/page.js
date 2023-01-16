032ac10dbeed6a0eecb919aa6619f74fdbe67c93
Koeunseooooo <rhdmstj17@cau.ac.kr>
2023-01-16 21:21:32 +0900

    fix: remove resolveAfter10sec func

--- app/step/4/page.js
+++ app/step/4/page.js
@@ -6,18 +6,6 @@ import { LetterButton } from "./components/LetterButton"
 import { LetterToName } from "./components/LetterToName"
 
 export default async function Step4() {
-    function resolveAfter10Seconds() {
-        // eslint-disable-next-line no-undef
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                resolve("resolved")
-            }, 10000)
-        })
-    }
-
-    // eslint-disable-next-line no-unused-vars
-    const result = await resolveAfter10Seconds()
-
     const nextCookies = cookies()
     const videoProps = JSON.parse(
         nextCookies.get("props")?.value ?? JSON.stringify({ name: "danpacho" })
