05ac58f66af167f90291f4dcbfde651130f5dda1
Danpa_cho <danpa725@cau.ac.kr>
2023-01-16 16:20:30 +0900

    feat: update step4 page for aws remotion video rendering

--- app/download/page.js
+++ app/download/page.js
@@ -1,16 +1,20 @@
 import { Button } from "../common/Button"
 
 /**
- * @param {{params: any, searchParams: { region: string, bucketName: string, from: string }}} param0
- * @returns
+ * @param {{params: any, searchParams: {renderId: string | null; bucketName: string | null; region: string | null; to: string}}} param0
  */
 export default function DownloadVideo({ searchParams }) {
-    const downloadUrl = `${searchParams}`
     const isInvalid =
-        !searchParams.bucketName || !searchParams.from || !searchParams.region
-    if (isInvalid) {
-        return <>오류가 발생했어요ㅠㅠ</>
-    }
+        !searchParams.bucketName ||
+        !searchParams.to ||
+        !searchParams.region ||
+        !searchParams.renderId
+
+    if (isInvalid) return <>오류가 발생했어요</>
+
+    const { bucketName, region, renderId } = searchParams
+    const downloadUrl = `https://s3.${region}.amazonaws.com/${bucketName}/renders/${renderId}/out.mp4`
+
     return (
         <>
             <a href={downloadUrl} download={"thanks clip"}>
--- app/step/4/page.js
+++ app/step/4/page.js
@@ -1,22 +1,30 @@
 import { cookies } from "next/headers"
-import { encodeVideo } from "../../../pages/api/rendering.js"
-// import { KakaoShareLink } from "./components/KakaoShareLink.jsx"
+import { COOKIE_KEY } from "../../../constant/cookie.js"
+import {
+    encodeVideo,
+    transformVideoProps,
+} from "../../../pages/api/rendering.js"
+import { Download } from "./components/Download.jsx"
 import { LetterButton } from "./components/LetterButton"
 import { LetterToName } from "./components/LetterToName"
 
 export default async function Step4() {
     const nextCookies = cookies()
-    const videoProps = JSON.parse(
-        nextCookies.get("props")?.value ?? JSON.stringify({ name: "danpacho" })
-    )
 
-    const encode = await encodeVideo(videoProps)
-    // eslint-disable-next-line no-unused-vars
-    const { bucketName, region, renderId } = encode
+    /**@type {import("../../../atoms/letter".Letter)} */
+    const videoProps = JSON.parse(nextCookies.get(COOKIE_KEY)?.value)
+    const transformedVideoProps = await transformVideoProps(videoProps)
+
+    const encode = await encodeVideo(transformedVideoProps)
+
     return (
         <>
             <LetterToName />
-            <LetterButton />
+            <LetterButton urlParams={{ ...encode, to: videoProps.to }} />
+            <Download
+                encode={encode}
+                transformedVideoProps={transformedVideoProps}
+            />
         </>
     )
 }
