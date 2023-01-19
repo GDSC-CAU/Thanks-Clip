44cae2ec942910749844e92685dcc7c597feee86
Danpa_cho <danpa725@cau.ac.kr>
2023-01-19 16:18:28 +0900

    feat: add logger

--- app/step/4/components/Download.jsx
+++ app/step/4/components/Download.jsx
@@ -21,7 +21,8 @@ const byteToSize = ({ byte, type = "kb", ceilOn = 1 }) => {
  * @param {{ encode: {renderId: string | null; bucketName: string | null; region: string | null;} }} downloadProps
  * @returns
  */
-const Download = ({ encode }) => {
+const Download = ({ encode, transformedVideoProps }) => {
+    console.log(transformedVideoProps, encode)
     /**@type {[Progress, Dispatch<SetStateAction<Progress>>]} */
     const [progress, setProgress] = useState({
         type: "progress",
