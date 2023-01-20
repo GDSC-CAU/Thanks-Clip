919760f0a78d7fc1c3d5da7a3af76bc575e0958a
Danpa_cho <danpa725@cau.ac.kr>
2023-01-21 00:44:13 +0900

    fix: async error at page4

--- app/step/4/page.js
+++ app/step/4/page.js
@@ -1,17 +1,7 @@
-import { cookies } from "next/headers"
-import { encodeVideo } from "../../../pages/api/rendering.js"
 import { LetterButton } from "./components/LetterButton"
 import { LetterToName } from "./components/LetterToName"
-import { KakaoShareLink } from "./components/KakaoShareLink.jsx"
 
-export default function Step4() {
-    const nextCookies = cookies()
-    const videoProps = JSON.parse(
-        nextCookies.get("props")?.value ?? JSON.stringify({ name: "danpacho" })
-    )
-
-    const encode = await encodeVideo(videoProps)
-
+export default async function Step4() {
     return (
         <>
             <LetterToName />
