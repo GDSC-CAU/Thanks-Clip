28ce7fa2ab561b1184953a8b3f6d1ab75b3ba4da
Danpa_cho <danpa725@cau.ac.kr>
2023-01-21 00:41:16 +0900

    refactor: move `Confetti` to components dir and refactor code

--- app/download/components/Confetti.jsx
+++ app/download/components/Confetti.jsx
@@ -1,11 +1,12 @@
 "use client"
+
 import { useCallback, useEffect, useRef, useState } from "react"
 import ReactCanvasConfetti from "react-canvas-confetti"
-import { useMounted } from "../step/RenderOnMount"
+import { useMounted } from "../../step/RenderOnMount"
 
-function randomInRange(min, max) {
-    return Math.random() * (max - min) + min
-}
+const randomInRange = (min, max) => Math.random() * (max - min) + min
+
+/**@type {React.CSSProperties} */
 const canvasStyles = {
     position: "fixed",
     pointerEvents: "none",
@@ -14,45 +15,52 @@ const canvasStyles = {
     top: 0,
     left: 0,
 }
-function getAnimationSettings(originXA, originXB) {
-    return {
-        startVelocity: 35,
-        spread: 360,
-        ticks: 500,
-        zIndex: 0,
-        particleCount: 80,
-        origin: {
-            x: randomInRange(originXA, originXB),
-            y: Math.random() - 0.2,
-        },
-    }
-}
-const Fireworks = () => {
+
+const getAnimationSettings = (originXA, originXB) => ({
+    startVelocity: 30,
+    spread: 500,
+    ticks: 500,
+    zIndex: 0,
+    particleCount: 50,
+    origin: {
+        x: randomInRange(originXA, originXB),
+        y: Math.random() - 0.2,
+    },
+})
+
+const Confetti = () => {
     const refAnimationInstance = useRef(null)
     const [intervalId, setIntervalId] = useState()
+
     const { isMounted } = useMounted()
+
     const getInstance = useCallback((instance) => {
         refAnimationInstance.current = instance
     }, [])
+
     const nextTickAnimation = useCallback(() => {
         if (refAnimationInstance.current) {
             refAnimationInstance.current(getAnimationSettings(0.1, 0.3))
             refAnimationInstance.current(getAnimationSettings(0.7, 0.9))
         }
     }, [])
+
     const startAnimation = useCallback(() => {
         if (!intervalId) {
             setIntervalId(setInterval(nextTickAnimation, 1500))
         }
     }, [intervalId, nextTickAnimation])
+
     useEffect(() => {
         return () => {
             clearInterval(intervalId)
         }
     }, [intervalId])
+
     useEffect(() => {
         if (isMounted === true) startAnimation()
-    }, [])
+    }, [isMounted, startAnimation])
+
     return (
         <>
             {isMounted ? (
@@ -64,4 +72,4 @@ const Fireworks = () => {
         </>
     )
 }
-export { Fireworks }
+export { Confetti }
