2b46230548ca2b55f84657a9e234c8dfbfb7bfb6
Danpa_cho <danpa725@cau.ac.kr>
2023-01-21 17:40:26 +0900

    feat: update download page link and layout

--- app/download/page.js
+++ app/download/page.js
@@ -4,25 +4,24 @@ import { Confetti } from "./components/Confetti"
 import { DownloadTitle } from "./components/DownloadTitle"
 import { Envelop } from "./components/Envelope"
 
+/**@typedef {{renderId: string | null; bucketName: string | null; region: string | null; from: string, to: string} DownloadURLParams} */
 /**
- * @param {{params: any, searchParams: {renderId: string | null; bucketName: string | null; region: string | null; from: string}}} param0
+ * @param {{params: any, searchParams: DownloadURLParams}} props
  */
 export default function DownloadVideo({ searchParams }) {
-    /** 실제 param 검증
     const isInvalid =
-        !searchParams.bucketName ||
+        !searchParams.to ||
         !searchParams.from ||
+        !searchParams.bucketName ||
         !searchParams.region ||
         !searchParams.renderId
-    */
-    const isInvalid = false
 
-    const { bucketName, region, renderId, from } = searchParams
+    const { bucketName, region, renderId, from, to } = searchParams
     const downloadUrl = `https://s3.${region}.amazonaws.com/${bucketName}/renders/${renderId}/out.mp4`
 
     if (isInvalid) {
         return (
-            <main className="flex flex-col w-full h-screen min-h-screen mx-auto sm:w-4/5 md:w-3/5 lg:w-2/5 px-3 py-3 bg-white ">
+            <main className="flex flex-col min-w-[22rem] w-full max-w-[30rem] h-screen min-h-screen mx-auto px-3 py-3 bg-white shadow-md">
                 <div className="h-full flex flex-col justify-between">
                     <DownloadTitle from={from} />
                     <div className="w-81 h-81 relative">
@@ -30,7 +29,7 @@ export default function DownloadVideo({ searchParams }) {
                     </div>
                     <div className="mt-auto mb-5">
                         <Button color="red">
-                            지금은 편지를 열 수 없어요 😭
+                            지금은 Clip을 열 수 없어요 😭
                         </Button>
                     </div>
                 </div>
@@ -39,16 +38,18 @@ export default function DownloadVideo({ searchParams }) {
     }
 
     return (
-        <main className="flex flex-col w-full h-screen min-h-screen mx-auto sm:w-4/5 md:w-3/5 lg:w-2/5 px-3 py-3 bg-white ">
+        <main className="flex flex-col min-w-[22rem] w-full max-w-[30rem] h-screen min-h-screen mx-auto px-3 py-3 bg-white shadow-md">
             <Confetti />
             <div className="h-full flex flex-col justify-between">
-                <DownloadTitle />
+                <DownloadTitle from={from} />
                 <div className="w-81 h-81 relative">
                     <Envelop />
                 </div>
                 <div className="mt-auto mb-5">
                     <a href={downloadUrl} download={"thanks clip"}>
-                        <Button className="mb-5 z-50">Clip 열어보기</Button>
+                        <Button className="mb-5 z-50">
+                            {to}님의 Clip 열어보기
+                        </Button>
                     </a>
                     <Link href="/step1/">
                         <Button className="bg-inherit text-gray-800 border-gray-800 border-solid border z-50">
