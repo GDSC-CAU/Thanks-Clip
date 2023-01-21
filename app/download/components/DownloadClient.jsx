3104cca8c3c325c5c3ebe8ee216e183cd918485d
Danpa_cho <danpa725@cau.ac.kr>
2023-01-21 20:29:01 +0900

    fix: move download page param server request to client using `useSearchParams`

---
+++ app/download/components/DownloadClient.jsx
@@ -0,0 +1,59 @@
+"use client"
+
+import Link from "next/link"
+import { useSearchParams } from "next/navigation"
+import { Button } from "../../common/Button"
+import { Confetti } from "./Confetti"
+import { DownloadTitle } from "./DownloadTitle"
+import { Envelop } from "./Envelope"
+
+const DownloadClientPage = () => {
+    const searchParams = useSearchParams()
+
+    const to = searchParams.get("to")
+    const from = searchParams.get("from")
+    const bucketName = searchParams.get("bucketName")
+    const renderId = searchParams.get("renderId")
+    const region = searchParams.get("bucketName")
+
+    const isValid =
+        searchParams.bucketName && searchParams.region && searchParams.renderId
+
+    const videoDownloadURL = `https://s3.${region}.amazonaws.com/${bucketName}/renders/${renderId}/out.mp4`
+
+    return (
+        <>
+            {isValid && <Confetti />}
+
+            <div className="h-full flex flex-col justify-between">
+                <DownloadTitle from={from} />
+
+                <div className="w-81 h-81 relative">
+                    <Envelop />
+                </div>
+
+                <div className="mt-auto mb-5">
+                    {isValid ? (
+                        <a href={videoDownloadURL} download={"thanks clip"}>
+                            <Button className="mb-5 z-50">
+                                {to}님의 Clip 열어보기
+                            </Button>
+                        </a>
+                    ) : (
+                        <Button color="red" className="mb-5 z-50">
+                            지금은 Clip을 열 수 없어요 😭
+                        </Button>
+                    )}
+
+                    <Link href="/step/1">
+                        <Button className="bg-inherit text-gray-800 border-gray-800 border-solid border z-50">
+                            저도 만들어볼래요 🙌
+                        </Button>
+                    </Link>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export { DownloadClientPage }
--- app/download/page.js
+++ app/download/page.js
@@ -1,63 +1,13 @@
-import Link from "next/link"
-import { Button } from "../common/Button"
-import { Confetti } from "./components/Confetti"
-import { DownloadTitle } from "./components/DownloadTitle"
-import { Envelop } from "./components/Envelope"
+import { DownloadClientPage } from "./components/DownloadClient"
 
 /**@typedef {{renderId: string | null; bucketName: string | null; region: string | null; from: string, to: string} DownloadURLParams} */
 /**
  * @param {{params: any, searchParams: DownloadURLParams}} props
  */
-export default function DownloadVideo({ searchParams }) {
-    const isInvalid =
-        !searchParams.to ||
-        !searchParams.from ||
-        !searchParams.bucketName ||
-        !searchParams.region ||
-        !searchParams.renderId
-    const { bucketName, region, renderId, from, to } = searchParams
-    const downloadUrl = `https://s3.${region}.amazonaws.com/${bucketName}/renders/${renderId}/out.mp4`
-    console.log(searchParams, downloadUrl)
-
-    if (isInvalid) {
-        return (
-            <main className="flex flex-col min-w-[22rem] w-full max-w-[30rem] h-screen min-h-screen mx-auto px-3 py-3 bg-white shadow-md">
-                <div className="h-full flex flex-col justify-between">
-                    <DownloadTitle from={from} />
-                    <div className="w-81 h-81 relative">
-                        <Envelop />
-                    </div>
-                    <div className="mt-auto mb-5">
-                        <Button color="red">
-                            지금은 Clip을 열 수 없어요 😭
-                        </Button>
-                    </div>
-                </div>
-            </main>
-        )
-    }
-
+export default function DownloadVideo() {
     return (
         <main className="flex flex-col min-w-[22rem] w-full max-w-[30rem] h-screen min-h-screen mx-auto px-3 py-3 bg-white shadow-md">
-            <Confetti />
-            <div className="h-full flex flex-col justify-between">
-                <DownloadTitle from={from} />
-                <div className="w-81 h-81 relative">
-                    <Envelop />
-                </div>
-                <div className="mt-auto mb-5">
-                    <a href={downloadUrl} download={"thanks clip"}>
-                        <Button className="mb-5 z-50">
-                            {to}님의 Clip 열어보기
-                        </Button>
-                    </a>
-                    <Link href="/step/1">
-                        <Button className="bg-inherit text-gray-800 border-gray-800 border-solid border z-50">
-                            저도 만들어볼래요 🙌
-                        </Button>
-                    </Link>
-                </div>
-            </div>
+            <DownloadClientPage />
         </main>
     )
 }
